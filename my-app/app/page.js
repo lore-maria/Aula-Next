f955406f2960995e169133d312073b9a5999a4ab
Lorena Maria <lorena.siqueira@academico.ifpb.edu.br>
2025-11-25 10:43:49 -0300

    atualizações

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,13 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
-      
         <div className="container">
           <h1>
             Bem-vindo à Biblioteca do IFPB!
           </h1>
+
+          <Image
+          src="/logoIF.png"
+          alt="IFPB Logo"
+          width={200}
+          height={300}/>
+
+          <p>
+            Explore nossa coleção de livros, artigos e recursos digitais. 
+          </p>
         </div>
+
+       
       
    
   );
